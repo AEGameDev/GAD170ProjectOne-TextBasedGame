47277abe757d4604fbbb6548c4848511953eb6f4
AEGameDev <aeganwork@gmail.com>
2024-10-08 13:43:22 +1000

    Added Pseudocode to Script

--- Assets/Scripts/TextBasedGame.cs
+++ Assets/Scripts/TextBasedGame.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class TextBasedGame : MonoBehaviour
 {
+
+    public int currentPlayerHealth = 100;
+    public int maxPlayerHealth = 100;
+    public int 
+    public int statPool = 20;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Game Begins - Player must mkae input to start
         
+        //Player Starts with 100HP
+
+        //Generate Random Stats From Stat Pool
+
+        //Announce Player Stats &Health
+
+        //Player Begins to Move Towards Enemies
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Enemy Appears/New Enemy Appears
+
+        //Player Attacks
+
+        //Attack Hits -- randomDamage
+
+        //Player Keeps Attacking Till Enemy Dead
+
+        //Enemy Dies
+
+        /*if
+        Player Levels Up - Give Player Stats + Health
+        else if
+        Player Level <5 New Enemy Appears // Player Level >=5 Player Wins GAME OVER!
+        */
+
+
     }
 }

08e1be7180d0c249e5ad1d12ff0a4a76031c0a86
AEGameDev <aeganwork@gmail.com>
2024-10-10 10:18:36 +1000

    Game Start Created, Player Attack, Enemy Spawn, Exp System & Level Up System Begun!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using JetBrains.Annotations;
+using UnityEngine.EventSystems;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,21 +14,90 @@ public class GameManager : MonoBehaviour
     public int playerHealth = 100;
     public int playerAttack = 10;
     public int playerLevel = 1;
-    public int enemyHealth = 100;
+    public int playerExperience = 0;
+    public float attackGain = 0.25f;
+
+    public int enemyHealth = 0;
     public int enemyAttack = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-      
-      mainText.text = "Your Character Level is " + playerLevel;
-      
+        Debug.Log("Welcome to the Game Adventurer!");
+        Debug.Log("It's time to slay some enemies!");
+        Debug.Log("When an enemy appears, press the A key to attack!");
+        Debug.Log("To begin your adventure, press B!");
+
+        enemyHealth += Random.Range(100, 150);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            Debug.Log("An enemy appears out of nowhere, you know what to do adventurer!");
+        }
 
+        Attack();
+        LevelUp();
+
+        if (enemyHealth < 1) 
+        {
+            EnemyKilled();
+            enemyHealth = Random.Range(100, 150);
+        } 
+
+   
+
+        //This is player attack function
+        void Attack()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                enemyHealth -= playerAttack;
+
+            }
+        }
+      
+        //This function logs when an enemy has been killed and updates player on current XP
+        void EnemyKilled()
+        {
+            playerExperience += Random.Range(0, 100);
+            Debug.Log("You slayed and enemy!");
+            Debug.Log("Your current XP is " + playerExperience + "/100XP");
+            
+        }
+
+        void LevelUp()
+        {
+            if (playerExperience >= 100)
+            {
+                Debug.Log("You can now level up!");
+                Debug.Log("Press L to increase level!");
+            }
+            if (Input.GetKeyDown (KeyCode.L))
+            {
+                playerLevel++;
+                Debug.Log("You are now level " + playerLevel + "!");
+                Debug.Log("A new enemy appears...");
+            }
+            if (playerExperience >= 100)
+            {
+                playerExperience = 0;
+            }
+        }
     }
+
+  
+    
+
+
+
+
+
+
+
+
 }
